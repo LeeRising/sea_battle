20a26ba1bc925f9781f7a9ec9d051ec0a3dcca47
LeeRising <leerising773@gmail.com>
2017-03-20 20:42:25 +0200

    UPD Changes

--- Morskoy_boy/UI/Forms/GameF.cs
+++ Morskoy_boy/UI/Forms/GameF.cs
@@ -14,6 +14,9 @@ namespace Morskoy_boy
     public partial class GameF : MaterialForm
     {
         int x1, x2, x3, x4;
+        string getPbName;
+        bool mb = false; //Нажатость левой кнопки мыши
+        int deltaX, deltaY; //Некоторая условная разность координат курсора относительно экрана и позиции объекта относительно границ формы-владельца
         public GameF()
         {
             InitializeComponent();
@@ -42,6 +45,11 @@ namespace Morskoy_boy
             x3countL.Text = x3.ToString() + " x:";
             x4countL.Text = x4.ToString() + " x:";
             groundBuilt();
+
+            _1Pb.Click += delegate { };
+            //_1Pb.MouseMove += delegate { };
+            //_1Pb.MouseDown += delegate { };
+
         }
         void groundBuilt()
         {
@@ -51,7 +59,7 @@ namespace Morskoy_boy
                 for (int j = 0; j < 10; j++)
                 {
                     groundCells[i, j] = new PictureBox();
-                    groundCells[i, j].Name = "groundCell" + i.ToString()+j.ToString();
+                    groundCells[i, j].Name = "groundCell" + i.ToString() + j.ToString();
                     groundCells[i, j].BackgroundImageLayout = ImageLayout.Stretch;
                     using (var f = File.OpenRead("app\\ships\\Simple\\_Nullpoint.png"))
                     {
@@ -62,6 +70,55 @@ namespace Morskoy_boy
                     groundPanel.Controls.Add(groundCells[i, j]);
                 }
             }
+            groundPanel.Controls.Add(new PictureBox
+            {
+                Name = "LettersPb",
+                BackgroundImageLayout = ImageLayout.Stretch,
+                Size = new Size(320, 32),
+                Location = new Point(38, 20)
+            });
+            groundPanel.Controls.Add(new PictureBox
+            {
+                Name = "NumsPb",
+                BackgroundImageLayout = ImageLayout.Stretch,
+                Size = new Size(32, 320),
+                Location = new Point(6, 52)
+            });
+            using (var f = File.OpenRead("app\\ships\\Simple\\Letters.png"))
+            {
+                groundPanel.Controls["LettersPb"].BackgroundImage = Image.FromStream(f);
+            }
+            using (var f = File.OpenRead("app\\ships\\Simple\\Nums.png"))
+            {
+                groundPanel.Controls["NumsPb"].BackgroundImage = Image.FromStream(f);
+            }
+        }
+        private void label1_MouseUp(object sender, MouseEventArgs e)
+        {
+            mb = false;//Неважно какая кнопка была отпущена, сбрасываем нажатость левой кнопки мыши, так проще управлять лейблом ИМХО
+            deltaX = 0;// Сбрасываем значения дельта
+            deltaY = 0;
+        }
+
+
+        private void label1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mb) //Если нажата и удерживается левая кнопка мыши
+            {
+                label1.Left = Cursor.Position.X - deltaX; //устанавливаем лейбл в новом месте относительно нового положения курсора экрана
+                label1.Top = Cursor.Position.Y - deltaY;
+            }
+        }
+
+        private void label1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left) //Если нажата левая кнопка мыши
+            {
+                mb = true; //Запоминаем статус нажатости
+                deltaX = Cursor.Position.X - label1.Location.X; //Запоминаем значения дельта
+                deltaY = Cursor.Position.Y - label1.Location.Y;
+            }
+
         }
     }
 }
