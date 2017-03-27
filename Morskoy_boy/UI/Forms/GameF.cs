21018f07c3749458d8d38d5a4a5cfa7740fc0de8
LeeRising <leerising773@gmail.com>
2017-03-27 09:20:08 +0300

    UPD Changes ships creater

--- Morskoy_boy/UI/Forms/GameF.cs
+++ Morskoy_boy/UI/Forms/GameF.cs
@@ -14,13 +14,13 @@ namespace Morskoy_boy
 {
     public partial class GameF : MaterialForm
     {
-        int x1, x2, x3, x4;
-        string getPbName,shipname;
+        int x1, x2, x3, x4, x_11;
+        string getPbName, shipname;
         public GameF()
         {
             InitializeComponent();
-#region Draw Ships Panel
-            using(var v = File.OpenRead("app\\ships\\Simple\\_1.png"))
+            #region Draw Ships Panel
+            using (var v = File.OpenRead("app\\ships\\Simple\\_1.png"))
             {
                 _1Pb.BackgroundImage = Image.FromStream(v);
             }
@@ -40,117 +40,90 @@ namespace Morskoy_boy
             x2 = 3;
             x3 = 2;
             x4 = 1;
-            x1countL.Text = x1.ToString()+" x:";
+            x1countL.Text = x1.ToString() + " x:";
             x2countL.Text = x2.ToString() + " x:";
             x3countL.Text = x3.ToString() + " x:";
             x4countL.Text = x4.ToString() + " x:";
-#endregion
+            #endregion
             BattleEngine.groundBuilt(this);
             Controls.Add(groundPanel);
 
             _1Pb.MouseEnter += delegate
             {
+                x_11 = 1;
                 getPbName = _1Pb.Name;
-                CreateBufferPb(x1,1);
+                CreateBufferPb(x1);
             };
             _2Pb.MouseEnter += delegate
             {
+                x_11 = 2;
                 getPbName = _2Pb.Name;
-                CreateBufferPb(x2,2);
+                CreateBufferPb(x2);
             };
             _3Pb.MouseEnter += delegate
             {
+                x_11 = 3;
                 getPbName = _3Pb.Name;
-                CreateBufferPb(x3,3);
+                CreateBufferPb(x3);
             };
             _4Pb.MouseEnter += delegate
             {
+                x_11 = 4;
                 getPbName = _4Pb.Name;
-                CreateBufferPb(x4,4);
+                CreateBufferPb(x4);
             };
-            //_1Pb.MouseLeave += delegate
-            //{
-            //    if (!mb)
-            //    {
-            //        getPbName = null;
-            //        Controls.Remove(bufferPb);
-            //        bufferPb = new PictureBox();
-            //    }
-            //};
-            //_2Pb.MouseLeave += delegate
-            //{
-            //    if (!mb)
-            //    {
-            //        getPbName = null;
-            //        Controls.Remove(bufferPb);
-            //        bufferPb = new PictureBox();
-            //    }
-            //};
-            //_3Pb.MouseLeave += delegate
-            //{
-            //    if (!mb)
-            //    {
-            //        getPbName = null;
-            //        Controls.Remove(bufferPb);
-            //        bufferPb = new PictureBox();
-            //    }
-            //};
-            //_4Pb.MouseLeave += delegate
-            //{
-            //    if (!mb)
-            //    {
-            //        getPbName = null;
-            //        Controls.Remove(bufferPb);
-            //        bufferPb = new PictureBox();
-            //    }
-            //};
-
         }
         PictureBox bufferPb;
         bool mb = false;
         int deltaX, deltaY;
-        public void CreateBufferPb(int x, int x1)
+        public void CreateBufferPb(int x)
         {
-            bufferPb = new PictureBox();
-            bufferPb.Name = shipname = x.ToString()+getPbName + "buffer";
-            bufferPb.BackgroundImageLayout = ImageLayout.Stretch;
-            bufferPb.BackgroundImage = Controls[getPbName].BackgroundImage;
-            bufferPb.Location = Controls[getPbName].Location;
-            bufferPb.Size = Controls[getPbName].Size;
-            Controls.Add(bufferPb);
-            Controls[bufferPb.Name].BringToFront();
-            bufferPb.MouseUp += Ships_MouseUp;
-            bufferPb.MouseDown += Ships_MouseDown;
-            bufferPb.MouseMove += Ships_MouseMove;
-            bufferPb.MouseEnter += Ships_MouseEnter;
-            bufferPb.MouseLeave += Ships_MouseLive;
-            switch (x1)
+            if (x > 0)
             {
-                case 1:
-                    if (x1 > 0)
-                        x1--;
-                    break;
-                case 2:
-                    if (x2 > 0)
-                        x2--;
-                    break;
-                case 3:
-                    if (x3 > 0)
-                        x3--;
-                    break;
-                case 4:
-                    if (x4 > 0)
-                        x4--;
-                    break;
+                bufferPb = new PictureBox();
+                bufferPb.Name = shipname = x.ToString() + getPbName + "buffer";
+                bufferPb.BackgroundImageLayout = ImageLayout.Stretch;
+                bufferPb.BackgroundImage = Controls[getPbName].BackgroundImage;
+                bufferPb.Location = Controls[getPbName].Location;
+                bufferPb.Size = Controls[getPbName].Size;
+                Controls.Add(bufferPb);
+                Controls[bufferPb.Name].BringToFront();
+                bufferPb.MouseUp += Ships_MouseUp;
+                bufferPb.MouseDown += Ships_MouseDown;
+                bufferPb.MouseMove += Ships_MouseMove;
+                bufferPb.MouseEnter += Ships_MouseEnter;
+                bufferPb.MouseLeave += Ships_MouseLive;
+                switch (x_11)
+                {
+                    case 1:
+                        if (x1 > 0)
+                            x1--;
+                        break;
+                    case 2:
+                        if (x2 > 0)
+                            x2--;
+                        break;
+                    case 3:
+                        if (x3 > 0)
+                            x3--;
+                        break;
+                    case 4:
+                        if (x4 > 0)
+                            x4--;
+                        break;
+                }
+                x1countL.Text = x1.ToString() + " x:";
+                x2countL.Text = x2.ToString() + " x:";
+                x3countL.Text = x3.ToString() + " x:";
+                x4countL.Text = x4.ToString() + " x:";
             }
         }
-#region ShipsMouseEvent
+        #region ShipsMouseEvent
         public void Ships_MouseUp(object sender, MouseEventArgs e)
         {
             mb = false;
             deltaX = 0;
             deltaY = 0;
-            getPbName = null;
         }
         public void Ships_MouseMove(object sender, MouseEventArgs e)
         {
@@ -176,8 +149,31 @@ namespace Morskoy_boy
         }
         public void Ships_MouseLive(object sender, EventArgs e)
         {
+            if (bufferPb.Location == Controls[getPbName].Location)
+            {
+                Controls.Remove(bufferPb);
+                switch (x_11)
+                {
+                    case 1:
+                        x1++;
+                        break;
+                    case 2:
+                        x2++;
+                        break;
+                    case 3:
+                        x3++;
+                        break;
+                    case 4:
+                        x4++;
+                        break;
+                }
+                x1countL.Text = x1.ToString() + " x:";
+                x2countL.Text = x2.ToString() + " x:";
+                x3countL.Text = x3.ToString() + " x:";
+                x4countL.Text = x4.ToString() + " x:";
+            }
             shipname = string.Empty;
         }
     }
-#endregion
+    #endregion
 }
